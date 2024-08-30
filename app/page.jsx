8d484c085072e83df40698a06d16ae241122bd0a
hooman.talakian@gmail.com <87539464+hoomantalakian@users.noreply.github.com>
2024-08-30 21:51:43 +0330

    home done

--- app/page.jsx
+++ app/page.jsx
@@ -9,29 +9,36 @@ export default function Home() {
 			{/* Banner */}
 			<section
 				id="home-banner"
-				className="flex gap-8 justify-center items-center bg-yellow-600 "
+				className="max-md:flex-col-reverse flex gap-10 justify-center items-center "
 			>
 				<Image
 					src="/my-face.jpg"
 					alt="my face"
 					width={500}
 					height={500}
-					className=" border-[1px] border-gray-500"
+					className="max-w-full w-[20rem] lg:w-[30rem] border-[4px] border-primary-medium shrink"
 				></Image>
 				{/* Text Box */}
-				<div id="banner_text" className="flex flex-col max-w-full">
+				<div
+					id="banner_text"
+					className="flex flex-col max-w-xl min-w-fit shrink-0"
+				>
 					<h1 id="my-name" className="mb-5">
 						I&apos;m{" "}
-						<span className="text-9xl font-extrabold text-gray-200">
-							Hooman Talakian,
+						<span className="text-6xl lg:text-8xl font-extrabold text-gray-200">
+							Hooman
+							<br />
+							Talakian,
 						</span>
 					</h1>
 					<p>
 						a professional{" "}
-						<span className="bold-span">Music Producer</span>, a
-						dedicated <span className="bold-span">Educator</span>, a
-						focused <span className="bold-span">Programmer</span>{" "}
-						and flexible{" "}
+						<span className="bold-span">Music Producer</span>,
+						<br />a dedicated{" "}
+						<span className="bold-span">Educator</span>,
+						<br />a focused{" "}
+						<span className="bold-span">Programmer</span> and
+						<br />a flexible{" "}
 						<span className="bold-span">Creative Leader</span>.
 					</p>
 				</div>
--- components/MainLayout.jsx
+++ components/MainLayout.jsx
@@ -2,7 +2,7 @@ function MainLayout({ children }) {
 	return (
 		<main
 			id="main-layout"
-			className="mt-[8em] bg-slate-600"
+			className="mt-[7rem] md:mt-[10em] max-sm:px-5 sm:px-[10rem] lg:px-[20rem]"
 		>
 			{children}
 		</main>
